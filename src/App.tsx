ce91c3998c773d0aaaf6fb315b00b26cea0ca8c1
Valentina Goncalves <mavagoncalves@gmail.com>
2026-07-30 16:04:26 +0100

    Add: Initial Folder Structure
    Added main features and components files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div className="flex h-screen items-center justify-center bg-gray-50">
+            <h1 className="text-3xl font-bold text-blue-600">App is working!</h1>
+          </div>
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
